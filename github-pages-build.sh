f5f6326486d3634ba151434c4fc81098cadf9a11
Amanda Easter <me@amandaeaster.com>
2024-03-27 22:09:34 -0500

    Updated bash and spotify dist

--- github-pages-build.sh
+++ github-pages-build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # Build Angular app for production
+echo 'Building Angular app for production...'
+
 ng build --prod --base-href ./
 
 # Navigate to the 'docs' directory
 cd docs
 
 # Copy 'index.html' to '404.html'
+echo 'Copying index into 404...'
 cp index.html 404.html
 
 # Create 'CNAME' file with specified content
+echo 'Updating CNAME'
 echo 'amandaeaster.com' > CNAME
 
 # Optional: Navigate back to the original directory
@@ -24,6 +28,7 @@ mkdir -p docs/spotify
 cd ../../spofity-comparison/dist/spotify-comparison
 
 # Copy all contents of the 'dist' folder into the newly created 'spotify' directory
+echo 'Copying Spotify Conversion Code...'
 cp -r * ../../../amandaeaster/amandaeaster/docs/spotify
 
 # Optionally, navigate back to the original directory
