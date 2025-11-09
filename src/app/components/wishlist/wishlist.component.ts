ba2e8fe90823130d9eac1785988259fbfeb80f39
Amanda Easter <me@amandaeaster.com>
2025-11-09 17:48:13 -0600

    Meta updates, new wishlist link

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -11,10 +11,10 @@ export class WishlistComponent {
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
-    const script = this.renderer.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=_3AwzZmQZKAHHpEHFJ-xpQ2&v=2';
-    script.id = 'script_myregistry_giftlist_iframe';
-    this.renderer.appendChild(this.el.nativeElement, script);
+    // const script = this.renderer.createElement('script');
+    // script.type = 'text/javascript';
+    // script.src = '//www.giftful.com/wishlists/t2VjQaGRtoKCvPfhEULA';
+    // script.id = 'script_myregistry_giftlist_iframe';
+    // this.renderer.appendChild(this.el.nativeElement, script);
   }
 }
